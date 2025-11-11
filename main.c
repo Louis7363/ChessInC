a2939bc3bd3cbfba23d1e378c0b2f090e6dbf92b
Louis7363 <louiscrt@sfr.fr>
2025-11-11 18:39:38 +0100

    ajout deplacement de la dame et restructure tour et fou pour dame, et dsl Arthur je n ai pas encore optimise...

--- main.c
+++ main.c
@@ -28,95 +28,110 @@ int* convertisseur_case(char position[2]) {
 
 }
 
+bool tour(int chess[8][8], int * depart,int * arrive) {
+    if (depart[0] == arrive[0]) {
+        bool avant = depart[1]<arrive[1];
+        int incr = 1;
+        if (!avant)
+            incr = -1;
+        for (int i=depart[1]+incr;i!=arrive[1];i+=incr) {
+            if (chess[depart[0]][i] != 0)
+                return false;
+
+        }
+
+        return true;
+    }
+    else if (depart[1]==arrive[1]) {
+        bool avant = depart[0]<arrive[0];
+        int incr = 1;
+        if (!avant)
+            incr = -1;
+        for (int i=depart[0]+incr;i!=arrive[0];i+=incr) {
+            if (chess[i][depart[1]] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    else
+        return false;
+
+}
+bool fou(int chess[8][8], int * depart,int * arrive) {
+    int n = abs(arrive[0] - depart[0]);
+    int n2 = abs(arrive[1]-depart[1]);
+    int s1=0;
+    int s2=0;
+    if (arrive[0] - depart[0]<0)
+        s1 = -1;
+    else
+        s1 = 1;
+    if (arrive[1] - depart[1]<0)
+        s2 = -1;
+    else
+        s2 = 1;
+
+    if (n == n2 ) {
+        for (int i = 1;i < n; i += 1) {
+            if (chess[depart[0] + i*s1][depart[1] + i*s2] != 0)
+                return false;
+
+        }
+        return true;
+    }
+
+    return false;
+}
+
 bool is_movement_possible (int chess[8][8], int * depart,int * arrive) {
     // ################ TOUR #################
-    if (chess[arrive[0]][arrive[1]] >0)
+    if (chess[arrive[0]][arrive[1]] >0) {
         return false;
+    }
 
     if (chess[depart[0]][depart[1]] == 4) {
 
-        if (depart[0] == arrive[0]) {
-            bool avant = depart[1]<arrive[1];
-            int incr = 1;
-            if (!avant)
-                incr = -1;
-            for (int i=depart[1]+incr;i!=arrive[1];i+=incr) {
-                if (chess[depart[0]][i] != 0)
-                    return false;
-
-            }
-
-            return true;
-        }
-        else if (depart[1]==arrive[1]) {
-            bool avant = depart[0]<arrive[0];
-            int incr = 1;
-            if (!avant)
-                incr = -1;
-            for (int i=depart[0]+incr;i!=arrive[0];i+=incr) {
-                if (chess[i][depart[1]] != 0) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        else
-            return false;
+        return(tour( chess,  depart, arrive));
     }
 
-    //################### FIN TOUR ##################
-    //################### PION ######################
-    if (chess[depart[0]][depart[1]] == 1) {
+        //################### FIN TOUR ##################
+        //################### PION ######################
+        if (chess[depart[0]][depart[1]] == 1) {
 
-        if (depart[1] == arrive[1]) {
-            int nbCases = depart[0]-arrive[0];
-            if (nbCases==1 && chess[arrive[0]][arrive[1]] == 0) {
-                return true;
+            if (depart[1] == arrive[1]) {
+                int nbCases = depart[0]-arrive[0];
+                if (nbCases==1 && chess[arrive[0]][arrive[1]] == 0) {
+                    return true;
+                }
+                else if (nbCases==2 && depart[0]==6 && chess[arrive[0]][arrive[1]] == 0 && chess[arrive[0]+1][arrive[1]] == 0) {
+                    return true;
+                }
+                else
+                    return false;
             }
-            else if (nbCases==2 && depart[0]==6 && chess[arrive[0]][arrive[1]] == 0 && chess[arrive[0]+1][arrive[1]] == 0) {
+            else if (arrive[0] == depart[0]-1 && (arrive[1] == depart[1]-1 || arrive[1] == depart[1]+1 ) && chess[arrive[0]][arrive[1]]<0) {
                 return true;
             }
             else
                 return false;
         }
-        else if (arrive[0] == depart[0]-1 && (arrive[1] == depart[1]-1 || arrive[1] == depart[1]+1 ) && chess[arrive[0]][arrive[1]]<0) {
-            return true;
-        }
-        else
-            return false;
-    }
 
-    //##################FIN PION############################
-    //################### FOU ##############################
-    if (chess[depart[0]][depart[1]] == 3) {
-        int n = abs(arrive[0] - depart[0]);
-        int n2 = abs(arrive[1]-depart[1]);
-        int s1=0;
-        int s2=0;
-        if (arrive[0] - depart[0]<0)
-            s1 = -1;
-        else
-            s1 = 1;
-        if (arrive[1] - depart[1]<0)
-            s2 = -1;
-        else
-            s2 = 1;
+        //##################FIN PION############################
+        //################### FOU ##############################
+        if (chess[depart[0]][depart[1]] == 3) {
 
-        if (n == n2 ) {
-            for (int i = 1;i < n; i += 1) {
-                    if (chess[depart[0] + i*s1][depart[1] + i*s2] != 0)
-                        return false;
+            return(fou( chess,  depart, arrive));
 
-            }
-            return true;
         }
 
-        return false;
-
-
+        //################ FIN FOU ###################
+        //################ DAME #####################
+        if (chess[depart[0]][depart[1]] == 5) {
+            return(fou( chess,  depart, arrive) || (tour( chess,  depart, arrive)));
+        }
     }
 
-}
 
 int main(void) {
     // pion : 1, cavalier : 2, fou : 3, tour : 4, dame: 5, roi : 6
